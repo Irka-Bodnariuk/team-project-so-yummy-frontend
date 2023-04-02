bf7afd6d3ac4ad27515681bc81967158542af65f
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2023-04-02 13:13:13 +0300

    add buttons size

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { ButtonStyled } from './Button.styled';
 
-export const Button = ({ children, look }) => {
+export const Button = ({ children, look, size }) => {
   return (
-    <ButtonStyled type="button" look={look}>
+    <ButtonStyled type="button" look={look} size={size}>
       {children}
     </ButtonStyled>
   );
