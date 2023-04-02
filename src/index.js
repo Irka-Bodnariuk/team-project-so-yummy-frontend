5cb421261bff8f2322a385af751c88bf31988bed
Oleksandr Kurzhy <103747805+voanergesKM@users.noreply.github.com>
2023-04-02 23:04:53 +0300

    added base routes

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import { App } from './Components/App/App';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from 'store/store';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/team-project-so-yummy-frontend'>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="/team-project-so-yummy-frontend">
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
