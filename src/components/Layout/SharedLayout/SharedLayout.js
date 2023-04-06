4267cb52a06925bf0d6b21bf33d41f60c52a4f3e
Oleksandr Kurzhy <103747805+voanergesKM@users.noreply.github.com>
2023-04-06 23:29:03 +0300

    added styles for layout and MainTitle sections

--- src/components/Layout/SharedLayout/SharedLayout.js
+++ src/components/Layout/SharedLayout/SharedLayout.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Footer from './../Footer/Footer';
 import { Outlet } from 'react-router-dom';
 import Header from './../Header/Header';
-
+import { LayoutContainer } from './SharedLayout.styled';
 
 export const SharedLayout = () => {
   return (
     <div>
-      <Outlet />
+      <LayoutContainer>
+        <Outlet />
+      </LayoutContainer>
       <Footer />
       <Header />
     </div>
