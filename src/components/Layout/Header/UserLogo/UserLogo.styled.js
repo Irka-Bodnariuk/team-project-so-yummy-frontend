d0b22543afd97d8fac7806f1d98f7b2d7aed3407
Iryna Bodnariuk <bodnariuk.iryna1@gmail.com>
2023-04-06 21:24:01 +0300

    add logo and nav

---
+++ src/components/Layout/Header/UserLogo/UserLogo.styled.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  
+`;
+
+export const WrapPhoto = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Photo = styled.div`
+  width: 34px;
+  height: 34px;
+  background-color: grey;
+  border-radius: 20px;
+  margin-right: 14px;
+`;
+export const Name = styled.p`
+  margin-right: 24px;
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 12px;
+  line-height: 170%;
+
+  color: #22252a;
+`;
--- src/components/Layout/SharedLayout/SharedLayout.js
+++ src/components/Layout/SharedLayout/SharedLayout.js
@@ -1,10 +1,12 @@
-import React from "react";
-import Footer from "./../Footer/Footer";
+import React from 'react';
+import Footer from './../Footer/Footer';
+import Header from './../Header/Header';
 
 export const SharedLayout = () => {
   return (
     <div>
       <Footer />
+      <Header />
     </div>
   );
 };
