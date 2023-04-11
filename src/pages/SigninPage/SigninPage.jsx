f21055de8597dc97c918ec06bf9cb236ce5c28d7
Oleksandr Kurzhy <103747805+voanergesKM@users.noreply.github.com>
2023-04-11 22:12:07 +0300

    added verification

--- src/pages/SigninPage/SigninPage.jsx
+++ src/pages/SigninPage/SigninPage.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { AuthForm } from 'components/AuthForm/AuthForm';
 import {
   AuthMain,
   AuthImage,
   AuthBackground,
 } from 'pages/RegisterPage/RegisterPage.styled';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 export const SigninPage = () => {
+  const params = useParams();
+
+  useEffect(() => {
+    const url =
+      'https://team-project-so-yummy-backend.onrender.com/api/auth/verify/';
+
+    if (params.verificationToken) {
+      axios.get(url.concat(params.verificationToken));
+    }
+  }, [params]);
+
   return (
     <AuthMain>
       <AuthImage></AuthImage>
