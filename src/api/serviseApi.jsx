1b7ce52a05918fbb14431fcb7e170bee386fe959
ImVladislav <nafanya102@gmail.com>
2023-04-12 15:16:52 +0300

    api update

--- src/api/serviseApi.jsx
+++ src/api/serviseApi.jsx
@@ -36,60 +36,18 @@ export const deleteMyRecipe = async id => {
   }
 };
 
-// export const updateSubscribe = async () => {
-//   try {
-//     const { data } = await axios.patch('/subscribe');
-//     return data;
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
+export const updateSubscribe = async () => {
+  try {
+    const { data } = await axios.patch('/subscribe');
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-
-// import axios from 'axios';
 
 // axios.defaults.baseURL = ' ';
 
-// export const getFavoriteRecipes = async page => {
-//   try {
-//     const { data } = await axios.get(`/favorites?page=${page}&limit=4`);
-//     return data.data;
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
-// export const deleteFavoriteRecipe = async id => {
-//   try {
-//     const { data } = await axios.put(`/favorites/${id}`);
-//     return data;
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
-// export const getMyRecipes = async page => {
-//   try {
-//     const { data } = await axios.get(
-//       `/recipes/own-recipes?page=${page}&limit=4`
-//     );
-//     return data.data;
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
-// export const deleteMyRecipe = async id => {
-//   try {
-//     const { data } = await axios.delete(`/recipes/own-recipes/${id}`);
-//     return data;
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
-
-//!             Popular recipe        //
 
 export const getPopularRecipes = async () => {
   try {
