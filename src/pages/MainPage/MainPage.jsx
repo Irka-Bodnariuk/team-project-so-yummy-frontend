07fff983269959ecd8cc5f7f18f8dca6d73d2705
the-most-trained-trainee <oleg.lavrenko@outlook.com>
2023-04-12 23:09:28 +0300

    main page background fixed

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -2,26 +2,24 @@ import PreviewCategories from 'components/PreviewCategories/PreviewCategories';
 import MainPageStyled from './MainPageStyled';
 import СhooseYourBreakfast from '../../components/СhooseYourBreakfast/СhooseYourBreakfast';
 import MainPageSearch from 'components/MainPageSearch/MainPageSearch';
-import { MainPageContainerStyled } from './MainPageStyled';
+// import { MainPageContainerStyled } from './MainPageStyled';
 
 const MainPage = () => {
   return (
     <>
-      <MainPageContainerStyled>
-        <MainPageStyled>
-          <h1>
-            <span>So</span>Yummy
-          </h1>
-          <p>
-            "What to cook?" is not only a recipe app, it is, in fact, your
-            cookbook. You can add your own recipes to save them for the future.
-          </p>
-          <div>
-            <СhooseYourBreakfast />
-            <MainPageSearch />
-          </div>
-        </MainPageStyled>
-      </MainPageContainerStyled>
+      <MainPageStyled>
+        <h1>
+          <span>So</span>Yummy
+        </h1>
+        <p>
+          "What to cook?" is not only a recipe app, it is, in fact, your
+          cookbook. You can add your own recipes to save them for the future.
+        </p>
+        <div>
+          <СhooseYourBreakfast />
+          <MainPageSearch />
+        </div>
+      </MainPageStyled>
       <PreviewCategories />
     </>
   );
