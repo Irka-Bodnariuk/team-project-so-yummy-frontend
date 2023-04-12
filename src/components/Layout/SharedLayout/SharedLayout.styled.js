fd72c702abe8b4ad1fdcf81b95b7d33e6b4410e1
PavloPSh <pavlopshe@gmail.com>
2023-04-12 22:00:02 +0300

    update

--- src/components/Layout/SharedLayout/SharedLayout.styled.js
+++ src/components/Layout/SharedLayout/SharedLayout.styled.js
@@ -4,14 +4,14 @@ export const LayoutContainer = styled.div`
   margin: 0 auto;
   padding-left: ${p => p.theme.space[4]}px;
   padding-right: ${p => p.theme.space[4]}px;
-  padding-top: ${p => p.theme.space[4] * 3}px;
+  padding-top: 114px;
   padding-bottom: 100px;
   min-width: 375px;
   background-color: ${p => p.theme.colors.background};
 
   @media screen and (min-width: 768px) {
     min-width: 768px;
-    padding-top: 73px;
+    padding-top: 136px;
     padding-left: ${p => p.theme.space[5]}px;
     padding-right: ${p => p.theme.space[5]}px;
     padding-bottom: 200px;
@@ -21,5 +21,6 @@ export const LayoutContainer = styled.div`
     max-width: 1440px;
     padding-left: 100px;
     padding-right: 100px;
+    padding-top: 164px;
   }
 `;
