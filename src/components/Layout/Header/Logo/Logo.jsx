8ab945dedeeff4490fc4399e500290ec30e360b8
Iryna Bodnariuk <bodnariuk.iryna1@gmail.com>
2023-04-12 10:41:25 +0300

    fix mob menu

--- src/components/Layout/Header/Logo/Logo.jsx
+++ src/components/Layout/Header/Logo/Logo.jsx
@@ -3,10 +3,15 @@ import { MainIcon } from 'images/svgIcons';
 
 import { NavLink } from 'react-router-dom';
 
-const Logo = () => {
-  const { isMobileScreen } = useMedia();
+const Logo = ({ handleClick }) => {
+  const { isMobileScreen, isDesktopScreen } = useMedia();
+  const onDesktop = () => {
+    if (!isDesktopScreen) {
+      handleClick();
+    }
+  };
   return (
-    <NavLink to="/main">
+    <NavLink onClick={() => onDesktop()} to="/main">
       <MainIcon mobile={isMobileScreen} />
     </NavLink>
   );
