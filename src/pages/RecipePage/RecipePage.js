269e8123e9587011f7ffc9f3f0703042338a347e
Iryna Bodnariuk <104754511+Irka-Bodnariuk@users.noreply.github.com>
2023-04-13 21:34:58 +0300

    api

--- src/pages/RecipePage/RecipePage.js
+++ src/pages/RecipePage/RecipePage.js
@@ -1,26 +1,26 @@
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import RecipeHero from 'components/RecipeHero/RecipeHero';
-import RecipeIngredients from 'components/RecipeIngredients/RecipeIngredients';
-import RecipeInstructions from 'components/RecipeInstructions/RecipeInstructions';
-import { getRecipeById } from 'api/services/axios/axiosService';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import RecipeHero from "components/RecipeHero/RecipeHero";
+import RecipeIngredients from "components/RecipeIngredients/RecipeIngredients";
+import RecipeInstructions from "components/RecipeInstructions/RecipeInstructions";
+import { getRecipeById } from "api/servisesApi";
 
 const RecipePage = () => {
   const { recipeId } = useParams();
-  const [recipe, setRecipe] = useState('');
+  const [recipe, setRecipe] = useState("");
   const [instructions, setInstructions] = useState([]);
   const [ingredients, setIngredients] = useState([]);
 
   useEffect(() => {
     getRecipeById(recipeId)
-      .then(res => {
+      .then((res) => {
         const { result } = res.data.data;
         setRecipe(result[0]);
         // console.log(result[0]);
-        setInstructions(result[0].instructions.split('. '));
+        setInstructions(result[0].instructions.split(". "));
         setIngredients(result[0].ingredients);
       })
-      .catch(error => console.error(error));
+      .catch((error) => console.error(error));
   }, [recipeId]);
   return (
     <div>
