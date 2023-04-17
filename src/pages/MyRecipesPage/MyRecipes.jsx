30b7f4e4c5244adddc3fecbbfd5f8213ae79de78
Oleksandr Kurzhy <103747805+voanergesKM@users.noreply.github.com>
2023-04-17 15:27:23 +0300

    Update MyRecipes.jsx

--- src/pages/MyRecipesPage/MyRecipes.jsx
+++ src/pages/MyRecipesPage/MyRecipes.jsx
@@ -2,13 +2,13 @@ import MyRecipesList from 'components/RecipesList/MyRecipesList';
 import MainTitle from 'components/MainTitle/MainTitle';
 import { Page } from '../FavoritePage/Favorite.styled';
 import BGDots from 'reusableComponents/BGDots/BGDots';
+import GoToTop from 'helpers/scrollToTop';
 
 const MyRecipes = () => (
   <Page>
     <BGDots />
     <MainTitle text="My recipes" />
     <MyRecipesList />
+    <GoToTop />
   </Page>
 );
-
-export default MyRecipes;
