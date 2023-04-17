acf14f22a309f7fcaa277f33dd5f16fbf43f52a2
Oleksandr Kurzhy <103747805+voanergesKM@users.noreply.github.com>
2023-04-17 14:05:03 +0300

    add paginate

--- src/components/FavoriteList/FavoriteList.jsx
+++ src/components/FavoriteList/FavoriteList.jsx
@@ -1,16 +1,17 @@
 import { useLayoutEffect, useState } from 'react';
-
 import { toast } from 'react-toastify';
-
 import { getFavoriteRecipes, deleteFavoriteRecipe } from 'api/index';
 import MyRecipeItem from '../RecipeItem/MyRecipeItem';
 import { Loader } from '../Loader/Loader';
 
 import { List, ListText, LoaderBox } from './FavoriteList.styled.js';
+import { Paginator } from 'components/Paginator/Paginator';
 
 const FavoriteList = () => {
   const [loading, setLoading] = useState(false);
   const [allRecipes, setAllRecipes] = useState([]);
+  const [currentItems, setCurrentItems] = useState([]);
+  const itemsPerPage = 4;
 
   const getFavorites = async () => {
     try {
@@ -39,29 +40,38 @@ const FavoriteList = () => {
   };
 
   return (
-    <List>
-      {loading && (
-        <LoaderBox>
-          <Loader />
-        </LoaderBox>
+    <>
+      <List>
+        {loading && (
+          <LoaderBox>
+            <Loader />
+          </LoaderBox>
+        )}
+        {currentItems.length !== 0 &&
+          !loading &&
+          currentItems.map(({ description, preview, time, title, _id }) => (
+            <MyRecipeItem
+              key={_id}
+              description={description}
+              preview={preview}
+              time={time}
+              title={title}
+              id={_id}
+              handleDelete={handleDelete}
+            />
+          ))}
+        {allRecipes.length === 0 && !loading && (
+          <ListText>You don't have your recipes</ListText>
+        )}
+      </List>
+      {!!allRecipes.length && (
+        <Paginator
+          totalItems={allRecipes}
+          perPage={itemsPerPage}
+          setCurrentItems={setCurrentItems}
+        />
       )}
-      {allRecipes.length !== 0 &&
-        !loading &&
-        allRecipes.map(({ description, preview, time, title, _id }) => (
-          <MyRecipeItem
-            key={_id}
-            description={description}
-            preview={preview}
-            time={time}
-            title={title}
-            id={_id}
-            handleDelete={handleDelete}
-          />
-        ))}
-      {allRecipes.length === 0 && !loading && (
-        <ListText>You don't have your recipes</ListText>
-      )}
-    </List>
+    </>
   );
 };
 
